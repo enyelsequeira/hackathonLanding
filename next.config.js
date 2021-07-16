9d29e46cbb6b9ab8dd3654feffd1d740144d6f95
Enyel Sequeira <enyelsequeira@hotmail.com>
2021-07-16 22:23:52 +0800

    almost done

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: true,
-  tabWidth: 2,
-  printWidth: 100,
-  trailingComma: "all",
-  jsxBracketSameLine: true,
-};
